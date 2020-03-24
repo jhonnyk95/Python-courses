e681cdb9cbaf0157a3894522d4ca755046876cc0
Jhonnyer Otalvaro <jhonnyer.o95@gmail.com>
2020-03-24 14:05:22 -0500

    Archivos

--- 16.proyecto.py
+++ 16.proyecto.py
@@ -1,10 +1,25 @@
 import os #Libreria para importar archivos
+
 #variable en mayuscula que significa que es una constante:
-CARPETA = 'contactos/' #Carpeta de contactos
+
+#Carpeta de contactos
+CARPETA = 'contactos/' 
+
+#Agrega la extension al archivo a crear
+EXTENSION = '.txt' 
+
+#Clase contacto
+class Contacto:
+    #Metodo contructor
+    def __init__(self, nombre, telefono, categoria):
+        self.nombre = nombre
+        self.telefono = telefono
+        self.categoria = categoria
 
 def app():
 
-    crear_directorio()##llamado de la funcion, revisa si la carpeta existe o no.
+    #llamado de la funcion, revisa si la carpeta existe o no.
+    crear_directorio()
 
     #Muestra el menu de opciones
     mostrar_menu()
@@ -12,13 +27,141 @@ def app():
     #preguntar al usuario la accion que desea
     preguntar = True
     while preguntar:
-        opcion = input(f'Seleccione una opción: \r\n')
+        opcion = input('Seleccione una opción: \r\n')
         opcion = int(opcion)
 
         #Ejecuta las opciones
         if opcion == 1:
-            print('Agregar contacto')
+            agregar_contacto()
             preguntar = False
+        elif opcion == 2:
+            editar_contacto()
+            preguntar = False
+        elif opcion == 3:
+            mostrar_contactos()
+            preguntar = False
+        elif opcion == 4:
+            buscar_contacto()
+            preguntar = False
+        elif opcion == 5:
+            eliminar_contacto()
+            preguntar = False
+        else:
+            print('Opcion no valida, Intente nuevamente')
+
+def eliminar_contacto():
+    nombre = input('Seleccione el Contacto que desea eliminar: \r\n')
+    try:
+        os.remove(CARPETA + nombre + EXTENSION)
+        print('Contacto eliminado corretamente')
+    except IOError:
+        print('No existe ese contacto')
+    
+    #Reiniciar app
+    app()
+
+
+def buscar_contacto():
+    nombre = input('Seleccione el Contacto que desea buscar: \r\n')
+
+    try:
+        with open(CARPETA + nombre + EXTENSION) as contacto:
+            print('\r\n Informacion del Contacto: \r\n')
+            for linea in contacto:
+                print(linea.rstrip())
+            print('\r\n')
+    except IOError:
+        print('El archivo no existe')
+        print(IOError)
+    
+    #reiniciar la app
+    app()
+    
+
+def mostrar_contactos():
+    archivos = os.listdir(CARPETA)
+
+    archivos_txt = [i for i in archivos if i.endswith(EXTENSION)]
+
+    for archivo in archivos_txt:
+        with open(CARPETA + archivo) as contacto:
+            for linea in contacto:
+                #imprime los contenidos
+                print(linea.rstrip())
+            #Imprime un separador entre contactos
+            print('\r\n')
+
+def editar_contacto():
+    print('Escribe el nombre de contacto a editar')
+    nombre_anterior = input('Nombre del contacto que desea editar: \r\n')
+    
+    #Revisar si el archivo existe antes de editarlo
+    existe = existe_contacto(nombre_anterior)
+
+    if existe:
+        with open(CARPETA + nombre_anterior + EXTENSION, 'w') as archivo:
+
+            #Resto de informacion
+            nombre_contacto = input('Agregar el Nuevo Nombre:\r\n')
+            telefono_contacto = input('Agregar el Nuevo Telefono:\r\n')
+            categoria_contacto = input('Categoria la Nueva Categoria:\r\n')
+
+            #instanciar
+            contacto = Contacto(nombre_contacto, telefono_contacto, categoria_contacto)
+
+            #Escribir el archivo
+            archivo.write('Nombre: ' + contacto.nombre + '\r\n' )
+            archivo.write('Telefono: ' + contacto.telefono + '\r\n' )
+            archivo.write('Categoria: ' + contacto.categoria + '\r\n' )
+
+            # Cerramos archivo
+            archivo.close()
+
+            #Renombrar el archivo
+            os.rename(CARPETA + nombre_anterior + EXTENSION, CARPETA + nombre_contacto + EXTENSION)
+
+            #Mostrar que el contacto se configuro exitosamente
+            print('\r\n Contacto editado correctamente \r\n')
+
+    else:
+        print('Ese contacto no existe')
+
+        #Reiniciar aplicacion
+        app()
+
+def agregar_contacto():
+    print('Escribe los datos para agregar el nuevo Contacto')
+    #variable para recibir el nombre
+    nombre_contacto = input('Nombre del Contacto: \r\n')
+
+    #Revisa si el archivo es igual o ya existe
+    existe = existe_contacto(nombre_anterior)
+
+    if not existe:
+        
+        #Para crear un archivo .txt con el nombre del contacto dentro de la carpeta
+        with open(CARPETA + nombre_contacto + EXTENSION, 'w') as archivo:
+
+            # Resto de la informacion
+            telefono_contacto = input('Agregar el Telefono:\r\n')
+            categoria_contacto = input('Categoria Contacto:\r\n')
+
+            #Instanciar la clase
+            contacto = Contacto(nombre_contacto, telefono_contacto, categoria_contacto)
+
+            #Escribir en el archivo
+            archivo.write('Nombre: ' + contacto.nombre + '\r\n' )
+            archivo.write('Telefono: ' + contacto.telefono + '\r\n' )
+            archivo.write('Categoria: ' + contacto.categoria + '\r\n' )
+
+            #Mensaje de exito
+            print('Contacto creado correctamente \r\n')
+    
+    else:
+        print('Ese contacto ya existe')
+    
+    #Reinicia la app
+    app()
 
 def mostrar_menu():
     print('Selecione del menu lo que desea hacer:')
@@ -36,5 +179,7 @@ def crear_directorio():
     else:
         print('La carpeta contactos ya existe')
 
+def existe_contacto(nombre):
+    return os.path.isfile(CARPETA + nombre + EXTENSION)
 
 app()
